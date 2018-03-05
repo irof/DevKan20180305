ea9119befc863dd3c0448fe6ffe8bfd729ce67b0
irof <irof@hogedriven.net>
2018-03-05 20:19:33 +0900

    タスクがとれる

---
+++ src/main/java/application/TodoService.java
@@ -0,0 +1,9 @@
+package application;
+
+import domain.Tasks;
+
+public class TodoService {
+    public Tasks todos() {
+        return new Tasks();
+    }
+}
---
+++ src/test/java/application/TodoServiceTest.java
@@ -0,0 +1,16 @@
+package application;
+
+import domain.Tasks;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TodoServiceTest {
+
+    @Test
+    void やることがとれる() {
+        TodoService sut = new TodoService();
+        Tasks actual = sut.todos();
+        assertThat(actual).isNotNull();
+    }
+}
